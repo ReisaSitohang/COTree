d679085ae841a3aa7e49c3226925e18033601eb9
Reisa Sitohang <reisasitohang@gmail.com>
2016-11-22 11:50:48 +0100

    skeleton

---
+++ src/app.js
@@ -0,0 +1,33 @@
+'use strict'
+//Import modules
+const express    =  require ( 'express' )
+const app        =  express( )
+const bodyParser =  require('body-parser')
+const Sequelize  =  require('sequelize')
+const session    =  require('express-session')
+const bcrypt     =  require('bcrypt-nodejs')
+const nodesass   = 
+const db         =  new Sequelize('cotree', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,{
+						host: 'localhost',
+						dialect: 'postgres'
+					});
+//Set views
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+//Use static folder
+app.use (express.static(__dirname + '/public'))
+
+
+//Setting Routes
+let loginlogoutRouter = require( __dirname+'/routes/login' )
+let registerRouter = require( __dirname+'/routes/register' )
+
+app.use('/', loginlogoutRouter )
+app.use('/', registerRouter )
+
+//Set port
+
+app.listen(3000, function () {
+	console.log('Yep Running')
+} )
