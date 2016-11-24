a2d678089a070feef0f8e54e6764122d1883f1bf
Manon Vrugt <manonvrugt@gmail.com>
2016-11-24 15:13:51 +0100

    one tree visible + mock data sent

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,10 +4,17 @@ const router = express.Router()
 
 router.get( '/home', (req, res) => {
 	console.log("Someone's home")
-
-	
-
-	res.render('home')
+	let data = {
+	donationAmount: 7,
+	fulltree: 7/2,
+	remainder: (7 % 2)/2
+	}
+	for (var i = 1; i < data.fulltree; i++) {
+		console.log("i looped " + i + "times!")
+	}
+	console.log(data.remainder)
+	// get user info
+	res.render('home', {data: data})
 })
 
 module.exports = router
