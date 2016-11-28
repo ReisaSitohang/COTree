e6a6ff31d1003b7970c634ccbc7ea64f694e235c
Reisa Sitohang <reisasitohang@gmail.com>
2016-11-28 17:55:48 +0100

    can select car on image added hover and on select opacity full

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1267528389987307', 
         'clientSecret'  : '2df2a7e6ac79fae591ec14707a6f7842', 
-        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     }
 } 
