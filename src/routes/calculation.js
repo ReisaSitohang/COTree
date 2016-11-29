02d723175616a56dfb48568cc6780a7299a31007
Reisa Sitohang <reisasitohang@gmail.com>
2016-11-29 11:47:32 +0100

    Posting donation data and kilometer data to Node

--- src/routes/calculation.js
+++ src/routes/calculation.js
@@ -3,16 +3,25 @@
 const express    =  require( 'express' )
 const router     =  express.Router(  )
 // const session    =  require( 'express-session' )
-// const bodyParser =  require( 'body-parser' )
+const bodyParser =  require( 'body-parser' )
 // const Sequelize  =  require( 'sequelize' )
 // const db         =  new Sequelize('cotree', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,{
 // 	host: 'localhost',
 // 	dialect: 'postgres'
 // });
 
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 //_________routes
 router.get('/calculation', function (req, res) {
 		res.render('calculationrdw')
+});
+
+router.post('/calculation', function (req, res) {
+		console.log("Donation: "+req.body.donation)
+		console.log("Kilometer: "+req.body.kilometer)
 });
 
 
