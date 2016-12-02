9b8402777377332d985c5989a8f8911093960ca7
Daniel van der Woude <c.d.a.vanderwoude@gmail.com>
2016-12-02 13:56:46 +0100

    login and facebook login done minor bug

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 'use strict'
-const express                     =  require ('express')
-const sequelize                   =  require('sequelize')
+const express                     = require ('express')
+const sequelize                   = require('sequelize')
 const passport                    = require('passport');
-const session                     =  require('express-session')
+const session                     = require('express-session')
 const router                      = express.Router()
 const Strategy                    = require('passport-facebook').Strategy;
 const LocalStrategy               = require('passport-local').Strategy;
-const init                        = require('../config/initialize.js')
-const bcrypt                      =  require('bcrypt-nodejs')
+const bcrypt                      = require('bcrypt-nodejs')
 const db                          = require('../models/database');
 const local                       = require('../models/local');
+const facebook                    = require('../models/facebook')
 
 
 router.use(session({
@@ -20,55 +20,56 @@ router.use(session({
 
 
 router.route('/')
- 	.get((req,res) => {
- 		res.render('index')
-  		}
-	);
+.get((req,res) => {
+	res.render('index')
+}
+);
 
 router.route('/index')
- 	.get((req,res) => {
- 		res.render('index')
-  		}
-	);
+.get((req,res) => {
+	res.render('index')
+}
+);
 router.route('/profile')
- 	.get((req,res) => {
- 		res.render('profile')
-  		}
-	);
+.get((req,res) => {
+	res.render('profile')
+}
+);
 
 
 router.route('/login')
-  .post(passport.authenticate('local', {    successRedirect: '/profile',
-                                   			failureRedirect: '/index',
-                                   			failureFlash: true }));
-
+.post(local.authenticate('local', {
+	successRedirect: '/profile',
+	failureRedirect: '/index'
+	})
+);
 
 
 router.route('/auth/facebook')
-	.get(passport.authenticate('facebook', {scope: ['public_profile', 'email']}))
+.get(facebook.authenticate('facebook', {scope: ['public_profile', 'email']}))
 
 
 router.route('/auth/facebook/callback')
-	.get(passport.authenticate('facebook', {
-		successRedirect: '/profile',
-		failureRedirect: '/index'
-	}))
+.get(facebook.authenticate('facebook', {
+	successRedirect: '/profile',
+	failureRedirect: '/index'
+}))
 
 router.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+	req.logout();
+	res.redirect('/');
+});
 
 router.route('/faq')
-	.get((req, res) => {
-		res.render('faq')
-	})
+.get((req, res) => {
+	res.render('faq')
+})
 
 router.route('/donate')
- 	.get((req,res) => {
- 		res.render('donate')
-  		}
-	);
+.get((req,res) => {
+	res.render('donate')
+}
+);
 
 
 ////////////////////////
@@ -76,22 +77,22 @@ router.route('/donate')
 //////////////////////
 
 router.route('/register')
-    .get((req,res)=> {
-        res.render('register')
-    })
-    .post((req,result)=> {
-        console.log(req.body)
-        bcrypt.hash(req.body.password, null, null, (err,hash) =>{
-            db.user.create({
-                name: req.body.first_name,
-                lastname: req.body.last_name,
-                email:req.body.email,
-                password: hash
-            }).then(()=>{
-                result.redirect('/?message=' +encodeURIComponent('Account created, you can now log-in'))
-            })
-        })
-    })
+.get((req,res)=> {
+	res.render('register')
+})
+.post((req,res)=> {
+	console.log(req.body)
+	bcrypt.hash(req.body.password, null, null, (err,hash) =>{
+		db.user.create({
+			name: req.body.first_name,
+			lastname: req.body.last_name,
+			email:req.body.email,
+			password: hash
+		}).then(()=>{
+			res.redirect('/?message=' +encodeURIComponent('Account created, you can now log-in'))
+		})
+	})
+})
 
 
 
