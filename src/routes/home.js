ee3e4b5447a8d8d240a456df5a706304aca70aeb
Manon Vrugt <manonvrugt@gmail.com>
2016-12-06 13:22:12 +0100

    trees now connected to backend through /totals

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,20 +2,23 @@
 const express = require( 'express' )
 const router = express.Router()
 
+
 router.get( '/home', (req, res) => {
 	console.log("Someone's home")
-	let donation = 2
-	let data = {
-	donationAmount: donation,
-	fulltree: donation/2,
-	remainder: ((donation % 2)/2)*100 + 'px'
-	}
-	for (var i = 1; i <= data.fulltree; i++) {
-		console.log("i looped " + i + "times!")
-	}
-	console.log(data.remainder)
-	// get user info
-	res.render('home', {data: data})
+	// MOCK DATA -- no longer in use
+
+	// let donation = 0
+	// let data = {
+	// // donationAmount: donation,
+	// // fulltree: donation/2,
+	// // remainder: ((donation % 2)/2)*100 + 'px'
+	// // }
+	// // for (var i = 1; i <= data.fulltree; i++) {
+	// // 	console.log("i looped " + i + "times!")
+	// // }
+	// // console.log(data.remainder)
+	// // get user info
+	// res.render('home', {data: data})
 })
 
 module.exports = router
