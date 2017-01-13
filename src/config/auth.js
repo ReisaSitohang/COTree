de88cc7ff6f5939cadf3c41a46da5657dbce3429
Daniel van der Woude <c.d.a.vanderwoude@gmail.com>
2017-01-13 21:50:10 +0100

    Update auth.js

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,8 +2,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1267528389987307', 
-        'clientSecret'  : '2df2a7e6ac79fae591ec14707a6f7842', 
+        'clientID'      : 'SECRET', 
+        'clientSecret'  : 'SECRET', 
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     }
 } 
